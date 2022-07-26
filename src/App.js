bb2d5be60a960265c44dd50180a3a30b737d6213
Gonzalo Nicotra <gonzalonicotra@hotmail.com>
2022-07-26 11:37:53 -0300

    v1.0

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         Proximamente Tienda MoldeArte
+         Proximamente Tienda MoldeArte!!
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.instagram.com/moldearteceramica/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Seguinos en Instagram
         </a>
       </header>
     </div>
