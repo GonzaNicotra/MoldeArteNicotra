8a19b56b6bbcd532c49ba0a5c7965b2ad3bcc8f3
Gonzalo Nicotra <gonzalonicotra@hotmail.com>
2022-08-20 02:15:44 -0300

    v1.3 more components

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   return (
     <div className="App">
       <Navbar/>
-      <ItemListContainer greeting="Bienvenido a MoldeArte"/>
+      <ItemListContainer/>
     </div>
   );
 }
--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,7 +1,17 @@
-import './ItemListContainer.css'
+import './ItemListContainer.css';
+import React from 'react';
+import ItemCount from '../ItemCount/ItemCount';
 
 export default function ItemListContainer({greeting}) {
+  
+  const onAdd = (counter) => {
+    console.log('el valor es:', counter);
+  }
+
   return (
-    <span className='titulo'>{greeting}</span>
+    <div>
+      <span className='titulo'>{greeting}</span>
+      <ItemCount stock={4} initial={0} onAdd={onAdd}/>
+    </div>
   )
 }
