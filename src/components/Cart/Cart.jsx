386282ce3320e34fec9709b4772ced9be0124788
Gonzalo Nicotra <gonzalonicotra@hotmail.com>
2022-09-13 20:39:40 -0300

    v1.11 item collection 2

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -4,6 +4,7 @@ import { CartContext } from '../../Context/CartContext';
 import { Link } from 'react-router-dom';
 import './Cart.css'
 import CartItems from '../CartItems/CartItems';
+import CartForm from '../CartForm/CartForm';
 
 export default function Cart() {
   const {items, clear} = useContext(CartContext)
@@ -26,12 +27,12 @@ export default function Cart() {
         <div>
         {items.map(((item, indx) => <CartItems key={indx} img={item.img} nombre={item.nombre} precio={item.precio} quantity={item.quantity} id={item.id}  />))}
         </div>
+        <div>
+        <CartForm/>
+      </div>
         <div>precio total = ${items.reduce((pv, cv) => pv + (cv.precio * cv.quantity) ,0)}</div>
         <div><Link to={'/productos'}><button className='bComprar'>Agregar más productos</button></Link></div>
         <div><button className='bComprar' onClick={clear}>Vaciar Carrito</button></div>
-      </div>
-      <div>
-        
       </div>
     </>
     }
--- src/components/CartForm/CartForm.jsx
+++ src/components/CartForm/CartForm.jsx
@@ -1,7 +1,33 @@
 import React from 'react'
+import Form from 'react-bootstrap/Form';
+
 
 export default function CartForm() {
+
+
+
   return (
-    <div>CartForm</div>
+    <Form className='col-sm-12 col-md-6 p-5 row d-flex justify-content-center text'>
+      <h2>Datos de Compra</h2>
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Nombre</Form.Label>
+        <Form.Control type="text" placeholder="Nombre" required/>
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Label>Apellido</Form.Label>
+        <Form.Control type="text" placeholder="Apellido" required/>
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Label>Email</Form.Label>
+        <Form.Control type="email" placeholder="name@example.com" required/>
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Label>Teléfono</Form.Label>
+        <Form.Control type="text" placeholder="Telefono" required/>
+      </Form.Group>
+      <button className='bComprar' type="submit">
+        Terminar Compra
+      </button>
+  </Form>
   )
 }
